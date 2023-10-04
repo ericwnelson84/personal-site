a2d0e4df7ab701faaa67f4177d8a8d216c4a2968
ericwnelson84 <130277514+ericwnelson84@users.noreply.github.com>
2023-10-03 19:30:55 -0500

    last

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template
 import requests
 import json
+import boto3
+from botocore.exceptions import ClientError
 
+
+
+
+secret_name = "news-api-key"
+region_name = "us-east-1"
+# Create a Secrets Manager client
+session = boto3.session.Session()
+client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name
+)
+
+try:
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+except ClientError as e:
+    # For a list of exceptions thrown, see
+    # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    raise e
+
+# Decrypts secret using the associated KMS key.
+secret = json.loads(get_secret_value_response['SecretString'])
+key = secret['x-api-key']
+
+print(key)
 
 app =Flask(__name__)
 
@@ -15,7 +43,7 @@ def news():
     url = "https://lly1yqp3p6.execute-api.us-east-1.amazonaws.com/Prod/newsreader"
     payload = "{\r\n  \"sentiment\": \"NEUTRAL\"\r\n}"
     headers = {
-        'x-api-key': 'RbhcMyrXwO4OYH0NXrjuA5MjefneI9Pv4Mw1CEGZ',
+        'x-api-key': key,
         'Content-Type': 'text/plain'
     }   
     response = requests.request("POST", url, headers=headers, data=payload)
@@ -28,4 +56,5 @@ def news():
 
 if __name__ == '__main__':
     app.run(debug=False)
+
 
